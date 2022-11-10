c1e14d9edcf34e7380a23f052e60abf25f432935
sagarKapoor95 <sagar.kapor95@gmail.com>
2022-11-10 11:33:59 -0500

    minor fixes

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chmod u+x ~/IdeaProjects/iot-smart-utility-backend/deploy.sh
+
+aws lambda update-function-code --function-name IOT_register_device --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_get_devices_for_hub --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_get_devices --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_create_resource_plan --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_get_resource_plan --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_register_central_iot_hub --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_User_Signup --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
+aws lambda update-function-code --function-name IOT_User_Login --zip-file fileb://build/distributions/iot-smart-utility-backend.zip --region us-east-1
